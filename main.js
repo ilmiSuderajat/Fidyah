53e50616bea016e2d83c459df1c2842c623be0b2
ilmiSuderajat <149875659+ilmiSuderajat@users.noreply.github.com>
2024-02-18 14:53:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,4 @@
 
-
-function lanang() {
-    var umur = document.getElementById("umur").value;
-    var input = document.getElementById("anak");
-    var hasilHari = document.getElementById("total")
-        var lanang = document.getElementById("laki");
-        var pr = document.getElementById("pr");
-        var label = document.getElementById("anak1");
-        var baleg1 = 15;
-        if (lanang.checked == true){
-            
-            input.style.display = "none";
-            label.style.display = "none";
-            function hasil(){
-                var umur = document.getElementById("umur").value;
-                umur -= baleg1
-                hasilHari.innerHTML = umur;
-            }
-        }else if (lanang.checked == false){
-
-            input.style.display = "flex";
-            label.style.display = "flex";
-
-        }else if (pr.checked == true){
-
-
-        }
-    }
     
     function hasil() {
         
@@ -35,14 +7,31 @@ function lanang() {
         var batesHaid = 53;
         var baleg = 9;
         var jumlahHaid = 84;
-        
-        var hasilUmur;
-        var hasilHari = document.getElementById("total")
-        umur = umur - baleg;
-        batesHaid -= baleg;
-        batesHaid = (batesHaid * jumlahHaid) + jumlahAnak * 40;
-        batesHaid = umur - (batesHaid / 360);
-        hasilUmur = Math.floor(batesHaid);
-            hasilHari.innerHTML= "Jumlah untuk membagikan fidyah adalah " + hasilUmur + " orang."
+         if( umur == ""){
+
+                alert("Masukan Umur Dipit")
+                hasilHari.style.display = "none"
+            }else if (jumlahAnak == ""){
+
+                alert("Masukan Jumlah Anak Dipit")
+            }
+            
+            
+            
+            else {
+
+
+                var hasilUmur;
+                var hasilHari = document.getElementById("total")
+                umur = umur - baleg;
+                batesHaid -= baleg;
+                batesHaid = (batesHaid * jumlahHaid) + jumlahAnak * 40;
+                batesHaid = umur - (batesHaid / 360);
+                hasilUmur = Math.floor(batesHaid);
+                    hasilHari.innerHTML= "Jumlah untuk membagikan fidyah adalah " + hasilUmur + " orang."
+
+            }
     
+
+           
     }
